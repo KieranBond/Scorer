504303225ecda5a8acbd6f8f364e2abfc7adfeb8
Kieran Bond <kieranbond97@gmail.com>
2022-10-25 19:50:51 +0100

    Basic user metric with stubbed data

---
+++ Scorer/UserMetrics.cs
@@ -0,0 +1,14 @@
+﻿namespace Scorer
+{
+    public sealed class UserMetrics
+    {
+        public readonly string Username;
+        public readonly string PageUrl;
+
+        public UserMetrics( string username, string pageUrl )
+        {
+            Username = username;
+            PageUrl = pageUrl;
+        }
+    }
+}
